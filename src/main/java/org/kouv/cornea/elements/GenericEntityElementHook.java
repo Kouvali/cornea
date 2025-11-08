40b741d9e76824b7b3782456031377b5c2546652
Kouvali <230672350+Kouvali@users.noreply.github.com>
2025-11-08 14:43:01 +0900

    Suppress unused warnings in GenericEntityElementHook

--- src/main/java/org/kouv/cornea/elements/GenericEntityElementHook.java
+++ src/main/java/org/kouv/cornea/elements/GenericEntityElementHook.java
@@ -3,6 +3,7 @@ package org.kouv.cornea.elements;
 import net.minecraft.entity.Entity;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("unused")
 public interface GenericEntityElementHook {
     @Nullable Entity cornea$getVelocityRef();
 
