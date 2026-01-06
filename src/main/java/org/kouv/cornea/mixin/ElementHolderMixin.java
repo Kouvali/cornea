aba7b150f8c6cee346fd49df356afb5e28a3296d
Kouvali <230672350+Kouvali@users.noreply.github.com>
2026-01-06 13:10:02 +0900

    Correct element tick execution timing

--- src/main/java/org/kouv/cornea/mixin/ElementHolderMixin.java
+++ src/main/java/org/kouv/cornea/mixin/ElementHolderMixin.java
@@ -171,20 +171,14 @@ public abstract class ElementHolderMixin implements ElementHolderHook {
             )
     )
     private void cornea$invokeTickListeners(CallbackInfo ci) {
-        for (TickListener listener : cornea$tickListeners) {
-            listener.onTick();
-        }
-    }
-
-    @Inject(
-            method = "tick",
-            at = @At(value = "TAIL")
-    )
-    private void cornea$invokeElementTickListeners(CallbackInfo ci) {
         for (VirtualElement element : elements.toArray(new VirtualElement[0])) {
             if (element instanceof AbstractElementHook hook) {
                 hook.cornea$triggerTickListeners();
             }
+        }
+
+        for (TickListener listener : cornea$tickListeners) {
+            listener.onTick();
         }
     }
 
