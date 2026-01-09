a1db65314766224df0f9495ae1681325447ac934
Kouvali <230672350+Kouvali@users.noreply.github.com>
2026-01-10 01:19:47 +0900

    Fix incorrect calculation in toDegrees extensions

--- src/main/kotlin/org/kouv/cornea/math/Math.kt
+++ src/main/kotlin/org/kouv/cornea/math/Math.kt
@@ -6,6 +6,6 @@ public fun Float.toRadians(): Float = Math.toRadians(this)
 
 public fun Double.toRadians(): Double = Math.toRadians(this)
 
-public fun Float.toDegrees(): Float = Math.toRadians(this)
+public fun Float.toDegrees(): Float = Math.toDegrees(this)
 
-public fun Double.toDegrees(): Double = Math.toRadians(this)
+public fun Double.toDegrees(): Double = Math.toDegrees(this)
