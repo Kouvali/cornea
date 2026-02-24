f594bbbd6416f755933a2bcaa64b0f8fa52bea00
Kouvali <230672350+Kouvali@users.noreply.github.com>
2026-02-25 06:04:10 +0900

    Remove deprecated trap functions for VirtualElement

--- src/main/kotlin/org/kouv/cornea/holders/ElementHolderExtensions.kt
+++ src/main/kotlin/org/kouv/cornea/holders/ElementHolderExtensions.kt
@@ -378,33 +378,3 @@ public inline fun ElementHolder.onTick(crossinline block: HolderTickScope.() -> 
     `cornea$addTickListener`(listener)
     return disposable
 }
-
-@Deprecated(
-    "Use org.kouv.cornea.elements.attributes instead",
-    level = DeprecationLevel.ERROR
-)
-public val VirtualElement.attributes: Attributes get() = error("Do not call")
-
-@Deprecated(
-    "Use org.kouv.cornea.elements.addAsPassengerTo instead",
-    level = DeprecationLevel.ERROR
-)
-public fun VirtualElement.addAsPassengerTo(entity: Entity): Unit = error("Do not call")
-
-@Deprecated(
-    "Use org.kouv.cornea.elements.onStartWatching instead",
-    level = DeprecationLevel.ERROR
-)
-public fun VirtualElement.onStartWatching(block: Nothing.() -> Unit): Unit = error("Do not call")
-
-@Deprecated(
-    "Use org.kouv.cornea.elements.onStopWatching instead",
-    level = DeprecationLevel.ERROR
-)
-public fun VirtualElement.onStopWatching(block: Nothing.() -> Unit): Unit = error("Do not call")
-
-@Deprecated(
-    "Use org.kouv.cornea.elements.onTick instead",
-    level = DeprecationLevel.ERROR
-)
-public fun VirtualElement.onTick(block: Nothing.() -> Unit): Unit = error("Do not call")
